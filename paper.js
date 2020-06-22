b33cf01b5e8755136da8a4fd28c86196ff65a972
sravaniGudimella <sravani.gudimella30@gmail.com>
2020-06-22 22:59:10 +0530

    Edited for test

--- paper.js
+++ paper.js
@@ -1,15 +1,23 @@
-class PaperClass{
-    constructor(x, y, width, height, angle) {
+class Paper{
+    constructor(x, y, radius) {
         var options = {
-            'restitution':0.2,
-            'friction':0.5,
-            'density':1.2
+            'restitution':0.8,
+            'friction':1.0,
+            'density':1.0
         }
-        this.body = Bodies.rectangle(x, y, width, height, options);
-        this.width = width;
-        this.height = height;
-        this.image = loadImage("sprites/base.png");
-        World.add(world, this.body);
-      }
+        this.body = Bodies.circle(x, y,radius, options);
+        this.radius = radius;
+        World.add(world,this.body);
      
       }
+      display(){
+        var angle = this.body.angle;
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rotate(angle);
+        ellipseMode(RADIUS);
+        ellipse( 0, 0, this.radius, this.radius);
+        pop();
+       
+      }
+}
--- sketch.js
+++ sketch.js
@@ -1,10 +1,11 @@
-var Dustbin1, dustbin2, dustbin3;
-var packageBody,ground
-var box1,box2,box3;
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
+
+var Dustbin1, dustbin2, dustbin3;
+var paper,ground;
+var box1,box2,box3;
 
 function preload()
 {
@@ -15,53 +16,34 @@ function setup() {
 	
 	createCanvas(800, 700);
 
-	box1=createSprite(600, 650, 100,20);
-	box2=createSprite(700, 610, 20,50);
-	box3=createSprite(800, 610,20,50);
-    box1.shapeColor = color ("red");
-    box2.shapeColor = color ("red");
-    box3.shapeColor = color ("red");
-   
-	paperBall=createSprite(width/2, 80, 10,10);
-
+	 
 	engine = Engine.create();
 	world = engine.world;
-
-	ground = new Ground(600,height,800,20)
 	
-	paperBall = Bodies.circle(width/2 , 200 , 5 , {restitution:1.5, isStatic:false});
-	World.add(world, packageBody);
-	
+	paper = new Paper(400, 500,5);
+	ground = new Ground(400,580,600,20);
+	box1 = new Box(680, 650, 100,20);
+	box2= new Box(620, 635, 20,50);
+	box3= new Box(720, 635,20,50);
 
-	//Create a Ground
-	ground = Bodies.rectangle(width/2, 650, width, 10 , {isStatic:true} );
- 	World.add(world, ground);
-
-	 box1 = Bodies.rectangle(450, 650, 200, 20 , {isStatic:true} );
-	 World.add(world, box1);
-
-	 box2 = Bodies.rectangle(350, 610, 20, 100 , {isStatic:true} );
-	 World.add(world, box2);
-
-	 box3 = Bodies.rectangle(350, 610, 20, 100 , {isStatic:true} );
-	 World.add(world, box3);
-
-
-  
 }
 
 
-function draw() {
-  rectMode(CENTER);
-  background(0);
-  
-  drawSprites();
- 
+function draw(){
+  background(255);
+  Engine.update(engine);
+  paper.display();
+  ground.display();
+  console.log("bye")
+ box1.display();
+  box2.display();
+  box3.display();
 }
-function keyPressed() {
+/*function keyPressed() {
 	if (keyCode === UP_ARROW) {
 
-	  Matter.Body.applyForce(paperBall.Body,paperBall.Body.position,{x:85,y:-85});
+	  Matter.Body.applyForce(paper.Body,paper.Body.position,{x:85,y:-85});
 	   
 	 }
 }
+*/
